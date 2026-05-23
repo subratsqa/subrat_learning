e4d621ec01349347ced03c143a75e8b004507076
Subrat Sahu <ssahu@dev.DataD.local>
2026-05-23 07:52:49 +0530

    Add for_loop, switch_case, while_loop, and 22thMay_task exercises

---
+++ switch_case.js/switch_case1.js
@@ -0,0 +1,59 @@
+let day = 4;
+switch (day) {
+    case 0:
+        console.log("Sunday- Rest day");
+        break;
+    case 1:
+        console.log("Monday - Sprint planning");
+        break;
+    case 2:
+        console.log("Tuesday - Devlopment");
+        break;
+    case 3:
+        console.log("Wedsday -Code review");
+        break;
+    case 4:
+        console.log("Thursday - Testing");
+        break;
+    case 5:
+        console.log("Friday - Deployement & Retro");
+        break;
+    case 6:
+        console.log("Saturday - Rest day");
+        break;
+    default:
+        console.log("Invalid day");
+
+}
+
+
+//Valaentines week
+let date = 14
+switch (date) {
+    case 7:
+        console.log("Rose day");
+        break;
+    case 8:
+        console.log("Propse day");
+        break;
+    case 9:
+        console.log("Chocolate day");
+        break;
+    case 10:
+        console.log("Teddy day");
+        break;
+    case 11:
+        console.log("Promise day");
+        break;
+    case 12:
+        console.log("Hug day");
+        break;
+    case 13:
+        console.log("Kiss day");
+        break;
+    case 14:
+        console.log("Valentine's day");
+        break;
+    default:
+        console.log("Not a valentine week day");
+}
